cd524bd234971c915bb61c7cf765ce63fce1089c
LkRonald <rpereirac@unicartagena.co>
2026-09-04 16:30:09 -0500

    feat(repository): se agrego el metodo closed a la clase DBConccion para cerrar la conexcion

--- src/main/java/com/company/proyecto_jsp/infrastructure/repository/DBConeccion.java
+++ src/main/java/com/company/proyecto_jsp/infrastructure/repository/DBConeccion.java
@@ -40,4 +40,10 @@ public class DBConeccion {
         }
         return conexion;
     }
+
+    public static void closeConnection() throws SQLException {
+        if (conexion != null) {
+            conexion.close();
+        }
+    }
 }
