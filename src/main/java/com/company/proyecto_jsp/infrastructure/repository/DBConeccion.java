74970d192e5005c6249d29b267ea3a28a14e7786
LkRonald <rpereirac@unicartagena.co>
2026-09-04 15:33:27 -0500

    feat(repository): se agrego la clase que hace la conexion con la base de datos

---
+++ src/main/java/com/company/proyecto_jsp/infrastructure/repository/DBConeccion.java
@@ -0,0 +1,31 @@
+package com.company.proyecto_jsp.infrastructure.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConeccion {
+
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_HOST = System.getenv("DB_HOST");
+    private static final String DB_NAME = System.getenv("DB_NAME");
+    private static final String DB_PASS = System.getenv("DB_PASS");
+    private static final String DB_PORT = System.getenv("DB_PORT");
+    private static final String DB_URL = "jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+    private static final String DRIVER = "com.postgresql.Driver";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new SQLException("Error: Driver PostgreSQL no encontrado.");
+        } catch (SQLException e) {
+            String mensaje = "Error: No se pudo establecer la conexion con la base de datos.";
+            throw new SQLException(mensaje);
+        }
+        return connection;
+    }
+}
